60f5565299fad49b49d15486ed45523155b40daf
twilight2017 <41724039@xs.ustb.edu.cn>
2020-05-20 17:40:25 +0800

    register

--- competition/controller/models.py
+++ competition/controller/models.py
@@ -3,19 +3,34 @@ from django.db import models
 # models用于和数据库交互，Django提供ORM，无须自行编写sql语句
 # Create your models here.
 
+
 class Students(models.Model):
+
     stu_id = models.CharField(max_length=50, default='None')
     stu_name = models.CharField(max_length=50, default='None')
-    stu_gender = models.BooleanField(default=False)
-    stu_grade = models.CharField(max_length=50,default='None')
-    stu_class = models.CharField(max_length=50,default='None')
-    stu_college = models.CharField(max_length=50,default='None')
-    stu_mail = models.CharField(max_length=50,default='None')
+    stu_gender = models.CharField(max_length=10, default=False)
+    identity = models.CharField(max_length=50, default='None')
+    stu_college = models.CharField(max_length=50, default='None')
+    stu_mail = models.CharField(max_length=50, default='None')
+    stu_password = models.CharField(max_length=50,default='None')
+    # 供于展示对象的函数
+
+    def to_dict(self):
+        return{
+            'stu_id': self.stu_id,
+            'stu_name': self.stu_name,
+            'stu_gender': self.stu_gender,
+            'identity': self.identity,
+            'stu_college': self.stu_college,
+            'stu_mail': self.stu_mail,
+        }
+
 
 class Admins(models.Model):
     adm_id = models.CharField(max_length=50,default='None')
     adm_name = models.CharField(max_length=50,default='None')
     adm_gender = models.BooleanField(max_length=50,default='None')
+    identity = models.CharField(max_length=50, default='None')
     adm_college = models.CharField(max_length=50,default='None')
     adm_mail = models.CharField(max_length=50,default='None')
 
--- competition/controller/views.py
+++ competition/controller/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Students, Admins
 
 # Create your views here.
+
+
+def register(request):
+    result = ''
+    if request.method == "POST" and request.POST:
+        # 判断是否为POST请求
+        #i_identity = request.POST.get('identity')
+        #if i_identity == "参赛人员":
+            u_id = request.POST.get('stu_id')
+            u_name = request.POST.get('stu_name')
+            u_gender = request.POST.get('stu_gender')
+            u_college = request.POST.get('stu_college')
+            u_mail = request.POST.get('stu_mail')
+            u_password = request.POST.get('stu_password')
+            u_pass = request.POST.get('stu_pass')
+            if u_password == u_pass:
+                db = Students.objects.filter(stu_id=u_id)
+                if db:
+                    result = 'had'
+                    # 该用户用户已注册过
+                    return render(request, 'register.html', {'result': result})
+                else:
+                    info = Students()
+                    info.stu_id = u_id
+                    info.stu_name = u_name
+                    info.gender = u_gender
+                    # info.identity = i_identity
+                    info.stu_college = u_college
+                    info.stu_mail = u_mail
+                    info.stu_password = u_password
+                    info.save()
+                    result = 'success'
+                    return render(request, 'register.html', {'result': result})
+            else:
+                result = 'error'
+                return render(request, 'register.html', {'result': result})
+    return render(request, 'register.html', {'result': result})
